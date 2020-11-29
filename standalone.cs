dd6cc11ec7a8e72295e33ec6412802d72f593b0c
Celeste Arantes <cro-marmot>
2020-11-28 21:30:37 -0600

    Adding files

---
+++ standalone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace root_namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bye bye World!");
+        }
+    }
+}
